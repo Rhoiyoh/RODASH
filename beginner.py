8dcb251b2cbe5cec76c07404368835342343aac2
Rhoiyoh <rhoiyoh@gmail.com>
2023-04-10 06:22:42 -0700

    Day 1 of my coding life

--- beginner.py
+++ beginner.py
@@ -1,1 +1,30 @@
-print("hello world")
+# Rodiat = 911
+# Isamotu = "rodiat"
+# name = {Rodiat, Isamotu}
+# print (name, Isamotu, Rodiat)
+
+if(1<0):
+    print("yes 0 is lesser")
+else:
+    print("no 1 is greater")
+    
+# asroph = 15
+# rodiat = 7
+# if(asroph>rodiat):
+#     print("yes asroph is greater")
+# else:
+#     print("no rodiat is lesser")
+
+yes = input("Is there light ")
+if(yes == "yes"):
+    yes = input("is the tutor around ")
+    if(yes == "yes"):
+        print("you can code")
+    else:
+        print("there is no need for the tutor to come")
+
+    
+elif(yes == "no"):
+    print("there is no need for the tutor to come")
+
+    
